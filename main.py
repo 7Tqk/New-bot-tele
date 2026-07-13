3cf07b79798f5163cd706104e164665db4c94d30
7Tqk <88940993+7Tqk@users.noreply.github.com>
2026-07-14 00:34:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ JOIN_CHANNEL_TARGET = get_valid_target(JOIN_CHANNEL_LINK, JOIN_CHANNEL_ID)
 JOIN_GROUP_TARGET = get_valid_target(JOIN_GROUP_LINK, JOIN_GROUP_ID)
 HITS_GROUP_TARGET = get_valid_target(HITS_GROUP_LINK, HITS_GROUP_ID)
 
-CHECKER_API_URL = 'https://web-production-3d364.up.railway.app/shopify'
+CHECKER_API_URL = 'https://web-production-7c318.up.railway.app/'
 GITHUB_SITES_URL = os.getenv("GITHUB_SITES_URL", "https://raw.githubusercontent.com/7Tqk/New-bot-tele/refs/heads/main/sites.txt")
 KEYS_FILE = "redeem_keys.json"
 

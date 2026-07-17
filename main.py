2b4c84516623c7df34ca70e3a7d989137bfdedbc
7Tqk <88940993+7Tqk@users.noreply.github.com>
2026-07-18 02:39:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ JOIN_GROUP_TARGET = get_valid_target(JOIN_GROUP_LINK, JOIN_GROUP_ID)
 HITS_GROUP_TARGET = get_valid_target(HITS_GROUP_LINK, HITS_GROUP_ID)
 
 # روابط الـ APIs النشطة (تمت ترقية رابط Shopify للجديد ببارامتراته بالكامل)
-SHOPIFY_API_URL_1 = 'https://shopy-kappa-nine.vercel.app/check'
+SHOPIFY_API_URL_1 = 'https://web-production-3d364.up.railway.app/shopify'
 AUTHNET_API_URL = 'https://authnet-4b3p.vercel.app/calc'
 GITHUB_SITES_URL = os.getenv("GITHUB_SITES_URL", "https://raw.githubusercontent.com/7Tqk/New-bot-tele/refs/heads/main/sites.txt")
 KEYS_FILE = "redeem_keys.json"
